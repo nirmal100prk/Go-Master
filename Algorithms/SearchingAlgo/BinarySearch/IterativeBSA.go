d6384983f469ade280403afc6c75e3df0be12bd6
npnirmal <npnirmal@dt.ae>
2024-08-18 22:11:40 +0400

    fix binary search

--- Algorithms/SearchingAlgo/BinarySearch/IterativeBSA.go
+++ Algorithms/SearchingAlgo/BinarySearch/IterativeBSA.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	fmt.Println(RecursiveBinarySearch(arr, 0, len(arr)-1, 2))
+	fmt.Println(RecursiveBinarySearch(arr, 0, 9, 7))
 }
 
 /*Time Complexity:
@@ -13,35 +13,35 @@ Average Case: O(log N)
 Worst Case: O(log N)*/
 
 // BSA only in sorted array
-func IterativeBinarySearch(arr []int, low, high, x int) int {
+func ItrBinarySearch(arr []int, val int) int {
+	low := 0
+	high := len(arr) - 1
 	for low <= high {
-		mid := (high - low) / 2
-
-		if arr[mid] == x {
+		mid := low + (high-low)/2
+		if arr[mid] == val {
 			return mid
 		}
 
-		if arr[mid] < x {
+		if arr[mid] < val {
 			low = mid + 1
-
 		} else {
 			high = mid - 1
 		}
-
 	}
 	return -1
+
 }
 
 func RecursiveBinarySearch(arr []int, low, high, x int) int {
 	for low <= high {
-		mid := (high - low) / 2
+		mid := low + (high-low)/2
 
 		if arr[mid] == x {
 			return mid
 		}
 
 		if arr[mid] < x {
-			return RecursiveBinarySearch(arr, low, mid+1, x)
+			return RecursiveBinarySearch(arr, mid+1, high, x)
 
 		} else {
 			return RecursiveBinarySearch(arr, low, mid-1, x)
