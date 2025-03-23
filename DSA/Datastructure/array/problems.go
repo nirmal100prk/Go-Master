a8e52e071af4b7726abd0c1a677c1bf7ce7b686a
npnirmal <npnirmal@dt.ae>
2025-03-23 15:30:59 +0400

    fix

--- DSA/Datastructure/array/problems.go
+++ DSA/Datastructure/array/problems.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 // insert element in a sorted array
 func InsertElement(data []int, val int) []int {
 	pos := FindInsertPosition(data, val)
@@ -25,4 +27,59 @@ func FindInsertPosition(data []int, val int) int {
 		}
 	}
 	return left
+}
+
+// GroupAnagrams
+
+// Input: strs = ["act","pots","tops","cat","stop","hat"]
+
+// Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+
+//o(n*klogk) - klogk - sorting ,
+func SortString(s string) string {
+	runes := []rune(s)
+	sort.Slice(runes, func(i, j int) bool { return runes[i] < runes[j] })
+	return string(runes)
+}
+
+func GroupAnagram(words []string) [][]string {
+
+	anagramMap := make(map[string][]string)
+	for _, word := range words {
+		sortedWord := SortString(word)
+		anagramMap[sortedWord] = append(anagramMap[sortedWord], word)
+	}
+
+	result := make([][]string, 0, len(anagramMap))
+	for _, v := range anagramMap {
+
+		result = append(result, v)
+	}
+	return result
+}
+
+
+// O(nlogn)
+func TopKFrequent(nums []int, k int) []int {
+	hashMap := make(map[int]int, 0)
+
+	for _, v := range nums {
+		hashMap[v]++
+	}
+
+	type pair struct {
+		num   int
+		count int
+	}
+	pairs := make([]pair, 0, len(hashMap))
+	for k, v := range hashMap {
+		pairs = append(pairs, pair{num: k, count: v})
+	}
+
+	sort.Slice(pairs, func(i, j int) bool { return pairs[i].count > pairs[j].count })
+	result := make([]int, k)
+	for i := 0; i < k; i++ {
+		result[i] = pairs[i].num
+	}
+	return result
 }
